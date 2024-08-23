d3a9a8b9689fbbc9ae386f9cd9f4df6b49160c88
oscar.castillo <oscar.castillo@LAGOCASTILLO>
2024-08-23 15:54:09 -0500

    xd

---
+++ Gcs.App/Models/ViewModels/VMRol.cs
@@ -0,0 +1,11 @@
+﻿using Gcs.Entity;
+
+namespace Gcs.App.Models.ViewModels
+{
+    public class VMRol
+    {
+        public int IdRol { get; set; }
+
+        public string? Descripcion { get; set; }
+    }
+}
