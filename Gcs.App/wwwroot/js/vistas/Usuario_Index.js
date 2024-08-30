f42518824aa155a2c6377e6dd7e420df825cd7f8
oscar.castillo <oscar.castillo@LAGOCASTILLO>
2024-08-30 14:34:22 -0500

    xd

--- Gcs.App/wwwroot/js/vistas/Usuario_Index.js
+++ Gcs.App/wwwroot/js/vistas/Usuario_Index.js
@@ -8,10 +8,10 @@
     urlFoto: ""
 }
 
-let tablaData;
 
-$(document).ready(function () {
-    tablaData = $('#tbdata').DataTable({
+
+function grid() {
+    let tablaData = $('#tbdata').DataTable({
         responsive: true,
          "ajax": {
              "url": '/Usuario/Lista',
@@ -19,17 +19,16 @@ $(document).ready(function () {
              "datatype": "json"
          },
          "columns": [
-             { "data": "idUsuario", "vivible": false, "searchable": false },
+             { "data": "idUsuario", "visible": false, "searchable": false },
              {
                  "data": "urlFoto", render: function (data) {
-                     return `<img style="height:60px" src=${data} class="rounded mx-auto d-block"/>`
-                 }
+                     return `<img style="height:60px" src=${data}/>`
+                 }, title: "Imagen"
              },
-             { "data": "nombre" },
-             { "data": "correo" },
-             { "data": "telefono" },
-             { "data": "nombreRol" },
-             { "data": "idRol" },                         
+             { "data": "nombre" , title: "Nombre"},
+             { "data": "correo", title: "Correo" },
+             { "data": "telefono", title: "Teléfono" },
+             { "data": "nombreRol", title: "Rol" },                        
              {
                  "data": "esActivo", render: function (data) {
                      if (data == 1) {
@@ -37,14 +36,14 @@ $(document).ready(function () {
                      } else {
                          return '<span class="badge badge-danger">No Activo</span>'  
                      }
-                 }
+                 }, title: "Estado"
              },
              {
                  "defaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class="fas fa-pencil-alt"></i></button>' +
                      '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>',
                  "orderable": false,
                  "searchable": false,
-                 "width": "80px"
+                 "width": "80px", title: "Acciones"
              }
          ],
          order: [[0, "desc"]],
@@ -64,4 +63,4 @@ $(document).ready(function () {
             url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
         },
     });
-});
+}
